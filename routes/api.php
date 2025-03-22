fd904c91d2dfadd60a3ffdcdbdfc30382c041dbd
Hannibal <06hannibal@gmail.com>
2025-03-22 19:48:25 +0200

    issue02: create api users

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
