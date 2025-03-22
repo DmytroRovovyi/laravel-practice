cc58da15c29c286cb2235edf53b9f9c659478c79
Hannibal <06hannibal@gmail.com>
2025-03-22 20:29:57 +0200

    issue03: create login api

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 
+// Create api users.
 Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{id}', [UserController::class, 'show']);
+Route::middleware('auth:sanctum')->get('/users/{id}', [UserController::class, 'show']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+Route::post('/login', [AuthController::class, 'login']);
