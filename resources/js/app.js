ddfbdbfd980302814da93ee848998c20fc4178c8
Hannibal <06hannibal@gmail.com>
2025-03-27 19:58:58 +0200

    issue08: create login web

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap';
 import '../sass/app.scss';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Головна сторінка</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home page</title>
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
 </head>
 <body>
@@ -17,15 +18,19 @@
                 <ul class="main-menu">
                     <li><a href="/about">Про нас</a></li>
                     <li><a href="/services">Послуги</a></li>
-                    <li><a href="/contact">Контакти</a></li>
+                    <li><a href="/dashboard">Dashboard</a></li>
                 </ul>
             </nav>
         </div>
         <div class="login-nav">
             <nav>
                 <ul class="login-menu">
-                    <li><a href="/login">login</a></li>
-                    <li><a href="/register">register</a></li>
+                    @guest
+                        <li><a href="/login">Login</a></li>
+                        <li><a href="/register">Register</a></li>
+                    @else
+                        <li><a href="/profile">Profile</a></li>
+                    @endguest
                 </ul>
             </nav>
         </div>
